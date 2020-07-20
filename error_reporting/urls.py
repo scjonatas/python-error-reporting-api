3fd625142663f424d454703770cd39d05fffbda5
Jonatas Correia <scjonatas@gmail.com>
2020-07-20 00:13:32 -0300

    Put the SwaggerUI documentation as the index of the app

--- error_reporting/urls.py
+++ error_reporting/urls.py
@@ -59,6 +59,6 @@ schema_view = get_schema_view(
 urlpatterns += [
     url(r'^api/', include(router.urls)),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
